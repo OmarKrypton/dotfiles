1131da73c55699048cf1d33a20dcc6bbc07345f7
Krypton <77502459+OmarKrypton@users.noreply.github.com>
2024-09-06 00:59:57 +0300

    Update setup_dotfiles.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -20,26 +20,29 @@ DOTFILES_DIR="$HOME/Downloads/dotfiles"
 
 # Check if the DOTFILES_DIR exists
 if [ ! -d "$DOTFILES_DIR" ]; then
-    echo "Error: Directory $DOTFILES_DIR does not exist."
+    echo -e "\e[31mError: Directory $DOTFILES_DIR does not exist.\e[0m"
     exit 1
 fi
 
+# Function to display a loading message and copy files
+copy_files() {
+    src="$1"
+    dest="$2"
+    description="$3"
+    echo -ne "Copying $description..."
+    mkdir -p "$dest"
+    cp -r "$src" "$dest" > /dev/null 2>&1
+    echo -e "\e[32mDone\e[0m"
+}
+
 # Copy .icons and .themes content
-echo "Copying icons and themes..."
-mkdir -p ~/.icons ~/.themes
-cp -vr "$DOTFILES_DIR/.icons/." ~/.icons/
-cp -vr "$DOTFILES_DIR/.themes/." ~/.themes/
-echo "Successfully copied icons and themes!"
+copy_files "$DOTFILES_DIR/.icons/." ~/.icons/ "icons"
+copy_files "$DOTFILES_DIR/.themes/." ~/.themes/ "themes"
 
 # Copy config content
-echo "Copying configuration files..."
-cp -vr "$DOTFILES_DIR/config/." ~/.config/
-echo "Successfully copied config files!"
+copy_files "$DOTFILES_DIR/config/." ~/.config/ "configuration files"
 
 # Copy wallpapers
-echo "Copying wallpapers..."
-mkdir -p ~/Pictures/wallpapers
-cp -vr "$DOTFILES_DIR/wallpapers/." ~/Pictures/wallpapers/
-echo "Successfully copied wallpapers!"
+copy_files "$DOTFILES_DIR/wallpapers/." ~/Pictures/wallpapers/ "wallpapers"
 
-echo "Dotfiles setup completed!"
+echo -e "\e[34mDotfiles setup completed!\e[0m"
